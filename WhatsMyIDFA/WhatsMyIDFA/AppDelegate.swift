a380da5bd2a19d3ea3370177782ad3ac87818ee5
CharlesWang <charleswang@taiwanmobile.com>
2017-11-14 13:57:21 +0800

    增加admob,podfile

--- WhatsMyIDFA/WhatsMyIDFA/AppDelegate.swift
+++ WhatsMyIDFA/WhatsMyIDFA/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+//        GADMobileAds.configure(withApplicationID: "ca-app-pub-7592415331992597~8303162590")
         return true
     }
 
--- WhatsMyIDFA/WhatsMyIDFA/ViewController.swift
+++ WhatsMyIDFA/WhatsMyIDFA/ViewController.swift
@@ -8,27 +8,31 @@
 
 import UIKit
 import AdSupport
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,GADBannerViewDelegate {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var idfaLabel: UILabel!
     @IBOutlet weak var osVersionLabel: UILabel!
     @IBOutlet weak var CopyButton: UIButton!
     
-    
     private var osVersion:String?
     private var idfa:String?
     
+    var bannerView: GADBannerView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.getDeviceInfo()
+        getDeviceInfo()
         NotificationCenter.default.addObserver(self, selector: #selector(getDeviceInfo), name: .UIApplicationDidBecomeActive, object: nil)
+        initBanner()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
-        self.setupUI()
+        setupUI()
     }
     
     override func didReceiveMemoryWarning() {
@@ -60,28 +64,98 @@ class ViewController: UIViewController {
         self.CopyButton.layer.masksToBounds = true
     }
     
+    func initBanner() {
+        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        addBannerViewToView(bannerView)
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716" //test
+//        bannerView.adUnitID = "ca-app-pub-7592415331992597/9323287140"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+    }
+    
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: bottomLayoutGuide,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+            ])
+    }
+    
     func copyToPasteBoard() {
         let pasteBoard = UIPasteboard.general
         pasteBoard.string = self.idfa
         SVProgressHUD.setMinimumDismissTimeInterval(0.1)
         SVProgressHUD.setDefaultStyle(SVProgressHUDStyle.light)
+        SVProgressHUD.setBackgroundColor(UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0))
         SVProgressHUD.showSuccess(withStatus: "Copied to pasteboard")
     }
     func goSetting() {
-        
+        var settingUrl =  URL(string: "App-Prefs:root=Privacy&path=ADVERTISING")
+        if (UIApplication.shared.canOpenURL(settingUrl!)){
+            
+        } else {
+            settingUrl = URL(string: UIApplicationOpenSettingsURLString)!
+        }
+        UIApplication.shared.openURL(settingUrl!)
     }
     
+    // MARK:- AdMob delegate
+    /// Tells the delegate an ad request loaded an ad.
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        print("adViewDidReceiveAd")
+        bannerView.alpha = 0
+        UIView.animate(withDuration: 1, animations: {
+            bannerView.alpha = 1
+        })
+    }
+    
+    /// Tells the delegate an ad request failed.
+    func adView(_ bannerView: GADBannerView,
+                didFailToReceiveAdWithError error: GADRequestError) {
+        print("adView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+    }
+    
+    /// Tells the delegate that a full screen view will be presented in response
+    /// to the user clicking on an ad.
+    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
+        print("adViewWillPresentScreen")
+    }
+    
+    /// Tells the delegate that the full screen view will be dismissed.
+    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
+        print("adViewWillDismissScreen")
+    }
+    
+    /// Tells the delegate that the full screen view has been dismissed.
+    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
+        print("adViewDidDismissScreen")
+    }
+    
+    /// Tells the delegate that a user click will open another app (such as
+    /// the App Store), backgrounding the current app.
+    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
+        print("adViewWillLeaveApplication")
+    }
+    
+    // MARK: - Button Action
     @IBAction func copyButtonPressed(_ sender: Any) {
         if (ASIdentifierManager.shared().isAdvertisingTrackingEnabled) {
-            self.copyToPasteBoard()
+            copyToPasteBoard()
         } else {
-            var settingUrl =  URL(string: "App-Prefs:root=Privacy&path=ADVERTISING")
-            if (UIApplication.shared.canOpenURL(settingUrl!)){
-                
-            } else {
-                settingUrl = URL(string: UIApplicationOpenSettingsURLString)!
-            }
-            UIApplication.shared.openURL(settingUrl!)
+            goSetting()
         }
     }
 }
